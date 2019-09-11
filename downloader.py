5c2f185887ce54398429e9ac4401e276a4edb8fd
Tyler Rose <tylerrose7930@gmail.com>
2019-09-11 11:31:19 +1000

    Added Youtube - Modified Entry

--- downloader.py
+++ downloader.py
@@ -8,14 +8,21 @@ clientsecret = ""
 # The Youtube API Auth Key.
 yt_key = ""
 
+# The Spotify Request Data
 data = f"{clientid}:{clientsecret}".encode("utf-8")
 
-playlistToDL = input("Spotify Playlist ID To Download: ")
+# The Playlist To Download
+gs = input("Google or Spotify: ").lower()
+playlistToDL = input("Playlist ID To Download: ")
+if 'g' in gs:
+    # Max Youtube Playlist Results
+    maxresults = input("Maximum Number of Items: ")
 
 
 def getSpotifyTracks(playlist_id):
     try:
         r = requests.get(f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks", headers={"Authorization": f"Bearer {sp_key}"})
+        print([x['track']['name'] for x in r.json()['items']], sep="\n")
         return r.json()
     except:
         return None
@@ -34,7 +41,6 @@ def youtube_api_search(query):
         if search_result["id"]["kind"] == "youtube#video":
             print(f"{query} : https://www.youtube.com/watch?v={search_result['id']['videoId']}")
             return "https://www.youtube.com/watch?v={}".format(search_result["id"]["videoId"])
-
 
 ar = requests.post(
                 "https://accounts.spotify.com/api/token", 
@@ -57,5 +63,18 @@ else:
 #with youtube_dl.YoutubeDL(ydl_opts) as ydl:
 #    ydl.download([x for x in trackURLS])
 
+videos = []
 with youtube_dl.YoutubeDL({}) as ytdl:
-    ytdl.download([x for x in [youtube_api_search(y) for y in [z['track']['name'] for z in getSpotifyTracks(playlistToDL)['items']]]])
+    try:
+        if 's' in gs:
+            ytdl.download([x for x in [youtube_api_search(y) for y in [z['track']['name'] for z in getSpotifyTracks(playlistToDL)['items']]]])
+        elif 'g' in gs:
+            files = f"https://www.googleapis.com/youtube/v3/playlistItems?key={yt_key}&part=snippet,contentDetails&maxResults={maxresults}&playlistId={playlistToDL}"
+            rq = requests.get(files).json()['items']
+            for video in rq:
+                print(f"{video['snippet']['title']} : https://youtu.be/{video['contentDetails']['videoId']}/")
+                videos.append(f"https://youtu.be/{video['contentDetails']['videoId']}/")
+            print(f"Items: {len(rq)}")
+            ytdl.download([video for video in videos])
+    except KeyError as ke:
+        print(f"Key Error: (Likely not enough queries left.) \n - {str(ke)}")
