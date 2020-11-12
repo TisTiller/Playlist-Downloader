76445b0e78d108709d5a455920f2ab0ef5e1052e
Tyler Rose <tylerrose7930@gmail.com>
2020-11-13 00:43:33 +1000

    Well that's easier isnt it.

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ This downloader uses youtube-dl to download playlists from spotify or youtube.
 It uses an api key that the user makes to request the url's.
 
 ## All
-- Needs Google Account
-  Create Youtube Api Key & Credentials in https://console.developers.google.com/
+- ~~Needs Google Account~~
+~~  Create Youtube Api Key & Credentials in https://console.developers.google.com/~~
 ## Spotify
 - Needs Spotify Account
   Make Spotify Bot/Api Key & Credentials in https://developer.spotify.com/dashboard/applications
@@ -14,5 +14,5 @@ It uses an api key that the user makes to request the url's.
 ### You Will Need:
 - Python >=3.5
 - Youtube-DL For that version
-- Accounts for spotify and/or youtube.
+- Accounts for spotify.
 
--- downloader.py
+++ downloader.py
@@ -7,7 +7,6 @@ with open("keys.json", 'r') as keys:
 
 clientid     = jsonvals['spotify']['clientid']
 clientsecret = jsonvals['spotify']['clientsecret']
-yt_key       = jsonvals['youtube']['yt_key']
 
 # The Spotify Request Data
 data = f"{clientid}:{clientsecret}".encode("utf-8")
@@ -28,47 +27,12 @@ def getSpotifyTracks(playlist_id):
     except:
         return None
 
-def youtube_api_search(query):
-    params = {"q": query, "part": "id", "key": yt_key, "maxResults": 1, "type": "video"}
-    yt_url = "https://www.googleapis.com/youtube/v3/search"
-    try:
-        r = requests.get(yt_url, params=params)
-
-        search_response = r.json()
-    except RuntimeError:
-        print("GONNA RETURN NONE")
-        return None
-    for search_result in search_response.get("items", []):
-        if search_result["id"]["kind"] == "youtube#video":
-            print(f"{query} : https://www.youtube.com/watch?v={search_result['id']['videoId']}")
-            return "https://www.youtube.com/watch?v={}".format(search_result["id"]["videoId"])
-
-ar = requests.post(
-                "https://accounts.spotify.com/api/token", 
-                data={"grant_type": "client_credentials"}, 
-                headers={"Authorization": f"Basic {str(b64encode(data), 'utf-8')}"}
-                ).json()
-if not ar['access_token']:
-    print("Didn't get an access token!")
-else:
-    sp_key = ar['access_token']
-
-
-# This Comment is equal to the last two lines.
-
-#trackNames = [x['track']['name'] for x in getSpotifyTracks("2YkD1uNBGI8BcBEOEOCf05")['items']]
-#trackURLS = []
-#for x in trackNames:
-#    trackURLS.append(youtube_api_search(x))
-#ydl_opts = {}
-#with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-#    ydl.download([x for x in trackURLS])
 
 videos = []
-with youtube_dl.YoutubeDL({}) as ytdl:
+with youtube_dl.YoutubeDL({"format": "bestaudio", 'default_search': 'ytsearch'}) as ytdl:
     try:
         if 's' in gs:
-            ytdl.download([x for x in [youtube_api_search(y) for y in [z['track']['name'] for z in getSpotifyTracks(playlistToDL)['items']]]])
+            ytdl.download([z['track']['name'] for z in getSpotifyTracks(playlistToDL)['items']])
         elif 'g' in gs:
             files = f"https://www.googleapis.com/youtube/v3/playlistItems?key={yt_key}&part=snippet,contentDetails&maxResults={maxresults}&playlistId={playlistToDL}"
             rq = requests.get(files).json()['items']
